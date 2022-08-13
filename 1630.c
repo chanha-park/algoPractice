ffdb1f44fb31c5cb9732d3bcb86346911ed2de7f
chanhpar <chanhpar@student.42seoul.kr>
2022-08-14 02:36:22 +0900

    Solve 1630

--- 1630.c
+++ 1630.c
@@ -34,28 +34,76 @@ int	is_prime(plist *primes, int n)
 {
 	list	*curr;
 
-	if (n > primes->tail->data)
+	curr = primes->head;
+	while (curr && curr->data <= n / curr->data)
 	{
-		curr = primes->head;
-		while (curr->data <= n / curr->data)
-		{
-			if (n % curr->data == 0)
-				return (0);
-		}
-		return (1);
+		if (n % curr->data == 0)
+			return (0);
+		curr = curr->next;
 	}
-	else
+	return (1);
+}
+
+int	get_exp(int n, int p)
+{
+	int	count = 0;
+
+	while (p <= n)
 	{
+		n /= p;
+		count += 1;
 	}
+	return (count);
+}
+
+int	crt(int a, int b, int c)
+{
+	int	r = c;
+
+	while (r % 289 != b)
+		r = (r + 379721) % 987654321;
+	while (r % 9 != a)
+		r = (r + 109739369) % 987654321;
+	return (r % 987654321);
 }
 
 int	oms(int n)
 {
 	plist	primes;
-	int	mod9 = mod289 = mod379721 = 1;
+	long long	mod9, mod289, mod379721;
+	int	exp;
 
-	primes->head = new_node(2);
-	primes->tail = primes->head;
+	if (n < 3)
+		return (n);
+	if (n == 3)
+		return (6);
+	if (n >= 379721)
+		return (0);
+	mod9 = mod289 = mod379721 = 1;
+	primes.head = new_node(2);
+	primes.tail = primes.head;
+	exp = get_exp(n, 2);
+	while (exp-- > 0)
+	{
+		mod9 = (mod9 * 2) % 9;
+		mod289 = (mod289 * 2) % 289;
+		mod379721 = (mod379721 * 2) % 379721;
+	}
+	for (int p = 3; p <= n; p += 2)
+	{
+		if (is_prime(&primes, p))
+		{
+			add_prime(&primes, p);
+			exp = get_exp(n, p);
+			while (exp-- > 0)
+			{
+				mod9 = (mod9 * (p % 9)) % 9;
+				mod289 = (mod289 * (p % 289)) % 289;
+				mod379721 = (mod379721 * (p % 379721)) % 379721;
+			}
+		}
+	}
+	return (crt(mod9, mod289, mod379721));
 }
 
 int	main(void)
