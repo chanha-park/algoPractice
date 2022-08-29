56b745f4fdb4a7a6d72c415fc481ff92a55ff985
chanha-park <chanhpar@student.42seoul.kr>
2022-08-29 20:05:38 +0900

    solve 2606 using recursion

--- 2606.c
+++ 2606.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// sol 1
+
+void	dfs(int **connect, int *visited, int n, int pop, int *count)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (connect[pop][i] == 1 && visited[i] == 0)
+		{
+			*count += 1;
+			visited[i] = 1;
+			dfs(connect, visited, n, i, count);
+		}
+	}
+}
+
+int	main(void)
+{
+	int	n, m, x, y;
+	int	**connect;
+	int	*visited;
+	int	count = 0;
+
+	scanf("%d", &n);
+	scanf("%d", &m);
+
+	connect = malloc(sizeof(int *) * n);
+	for (int i = 0; i < n; i++)
+		connect[i] = calloc(n, sizeof(int));
+	for (int i = 0; i < m; i++)
+	{
+		scanf("%d %d", &x, &y);
+		connect[x - 1][y - 1] = 1;
+		connect[y - 1][x - 1] = 1;
+	}
+	visited = calloc(n, sizeof(int));
+	visited[0] = 1;
+	dfs(connect, visited, n, 0, &count);
+	printf("%d\n", count);
+	for (int i = 0; i < n; i++)
+		free(connect[i]);
+	free(connect);
+	free(visited);
+	return (0);
+}
+
+/*
+// sol 2
 int	main(void)
 {
 	int	n, m, x, y;
@@ -48,3 +95,4 @@ int	main(void)
 	free(visited);
 	return (0);
 }
+*/
