f671a4ad3fa1f049b3297f399e4c877c91bfc308
chanha-park <chanhpar@student.42seoul.kr>
2022-09-05 11:37:36 +0900

    solve 14852 - iteration

--- 14852.c
+++ 14852.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
-int	f(int n, int a, int b, int c)
-{
-	if (n == 0)
-		return (a);
-	if (n == 1)
-		return (b);
-	if (n == 2)
-		return (c);
-	return (f(n - 1, b, c, ((long long)3 * c + b - a + 1000000007) % 1000000007));
-}
+/* int	f(int n, int a, int b, int c)                                                  */
+/* {                                                                                  */
+/*     if (n == 0)                                                                    */
+/*         return (a);                                                                */
+/*     if (n == 1)                                                                    */
+/*         return (b);                                                                */
+/*     if (n == 2)                                                                    */
+/*         return (c);                                                                */
+/*     return (f(n - 1, b, c, ((long long)3 * c + b - a + 1000000007) % 1000000007)); */
+/* }                                                                                  */
 
 int	main(void)
 {
 	int	n;
+	int	a, b, c, tmp;
 
 	scanf("%d", &n);
-	printf("%d\n", f(n, 1, 2, 7));
+	a = 0;
+	b = 1;
+	c = 2;
+	while (--n)
+	{
+		tmp = c;
+		c = ((long long)3 * c + b - a + 1000000007) % 1000000007;
+		a = b;
+		b = tmp;
+	}
+	printf("%d\n", c);
 	return (0);
 }
