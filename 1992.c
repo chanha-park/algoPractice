27038912bfd2956e72209d7faaa8dd472b155a54
chanha-park <chanhpar@student.42seoul.kr>
2022-09-08 12:56:18 +0900

    solve 1992

--- 1992.c
+++ 1992.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_qtree
+int	is_same(char **map, int r, int c, int size)
 {
-	int				data;
-	struct s_qtree	*leftup;
-	struct s_qtree	*rightup;
-	struct s_qtree	*leftdown;
-	struct s_qtree	*rightdown;
-}	qtree;
+	char	k = map[r][c];
 
-void	print_qtree(qtree *node)
+	for (int i = r; i < r + size; i++)
+		for (int j = c; j < c + size; j++)
+			if (k != map[i][j])
+				return (0);
+	return (1);
+}
+
+void	f(char **map, int r, int c, int size)
 {
-	if (!node)
-		return ;
-	if (node->left)
+	if (is_same(map, r, c, size))
+		printf("%c", map[r][c]);
+	else
 	{
+		size >>= 1;
 		printf("(");
-		print_qtree(node->leftup);
-		print_qtree(node->rightup);
-		print_qtree(node->leftdown);
-		print_qtree(node->rightdown);
+		f(map, r, c, size);
+		f(map, r, c + size, size);
+		f(map, r + size, c, size);
+		f(map, r + size, c + size, size);
 		printf(")");
 	}
-	else
-		printf("%d", node->data);
+}
+
+int	main(void)
+{
+	int	n;
+	char	**map;
+
+	scanf("%d", &n);
+	map = malloc(sizeof(char *) * (n + 1));
+	for (int i = 0; i < n; i++)
+	{
+		map[i] = malloc(sizeof(char) * (n + 1));
+		scanf("%s", map[i]);
+	}
+	map[n] = NULL;
+	f(map, 0, 0, n);
+	printf("\n");
+	return (0);
 }
