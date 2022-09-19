c20c6d464fe70882625d772a77c40f0d9aa6541b
chanha-park <chanhpar@student.42seoul.kr>
2022-09-19 13:40:00 +0900

    solve 15651 15652

---
+++ 15652.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define MAX 8
+
+int	n, m;
+int	arr[MAX];
+int	idx;
+
+void	recur()
+{
+	int	tmp;
+	int	curr;
+
+	if (idx == m)
+	{
+		for (int i = 0; i < m; i++)
+			printf("%d ", arr[i]);
+		printf("\n");
+	}
+	else
+	{
+		tmp = idx;
+		curr = arr[tmp - 1];
+		for (int i = curr; i <= n; i++)
+		{
+			idx = tmp;
+			arr[idx++] = i;
+			recur();
+		}
+	}
+}
+
+int	main(void)
+{
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++)
+	{
+		idx = 0;
+		arr[idx++] = i;
+		recur();
+	}
+	return (0);
+}
