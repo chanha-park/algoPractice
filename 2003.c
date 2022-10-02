de75a65b9dd1e9c7b978517b63e433031bcb5fdc
chanhpar <chanhpar@student.42seoul.kr>
2022-10-02 23:35:13 +0900

    solve 2003

--- 2003.c
+++ 2003.c
@@ -3,14 +3,22 @@
 
 int	cmp(const void *a, const void *b)
 {
-	return (*(int *)a > *(int *)b);
+	int	x = *(long *)a;
+	int	y = *(long *)b;
+
+	if (x > y)
+		return (1);
+	else if (x < y)
+		return (-1);
+	else
+		 return (0);
 }
 
 int	main(void)
 {
 	int	n, m;
 	long	*arr;
-	int		tmp;
+	long	tmp;
 	int		count = 0;
 
 	scanf("%d %d", &n, &m);
@@ -18,13 +26,13 @@ int	main(void)
 	arr[0] = 0;
 	for (int i = 0; i < n; i++)
 	{
-		scanf("%d", &tmp);
+		scanf("%ld", &tmp);
 		arr[i + 1] = arr[i] + tmp;
 	}
 	for (int i = 1; i < n + 1; i++)
 	{
 		tmp = arr[i] - m;
-		if (bsearch((const void *)&tmp, arr, i, sizeof(int), cmp))
+		if (bsearch((const void *)&tmp, arr, i, sizeof(long), cmp))
 			count++;
 	}
 	printf("%d\n", count);
