41413ad2a925fd5fa21a7c7b1b733a5e78a932e3
chanhpar <chanhpar@student.42seoul.kr>
2022-12-11 15:30:52 +0900

    Make 1850 faster

--- 1850.c
+++ 1850.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 char s[10000001];
 
-int gcd(long a, long b) {
-  if (a < b)
-    return (gcd(b, a));
-  if (a % b == 0)
-    return (b);
-  return (gcd(b, a % b));
-}
-
 int main(void) {
-  long a, b;
+  long a, b, c;
   scanf("%ld %ld", &a, &b);
-  memset(s, '1', gcd(a, b));
-  printf("%s\n", s);
+  c = a % b;
+  while (c) {
+    a = b;
+    b = c;
+    c = a % b;
+  }
+  memset(s, '1', b);
+  write(1, s, b);
   return (0);
 }
