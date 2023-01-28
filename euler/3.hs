8eb628935009f25382804338dfa51bfe11d80238
chanhpar <91934407+chanha-park@users.noreply.github.com>
2023-01-29 01:41:26 +0900

    Create 3.hs

---
+++ euler/3.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_GHC -Wall -Wextra -Werror #-}
+
+main :: IO ()
+main = print $ maxFactor 600851475143 2
+
+-- min factor s.t. >= p
+minFactor :: Integer -> Integer -> Integer
+minFactor n p
+    | p * p > n = n
+    | mod n p == 0 = p
+    | otherwise = minFactor n (p + 1)
+
+-- max factor s.t. >= p
+maxFactor :: Integer -> Integer -> Integer
+maxFactor n p
+    | k == n = n
+    | k == p = maxFactor (div n p) p
+    | otherwise = maxFactor n (p + 1)
+        where k = minFactor n p
