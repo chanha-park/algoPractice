137a937f26d2145d947cc5f689bbf07010977c97
chanhpar <chanhpar@student.42seoul.kr>
2023-02-05 19:25:44 +0900

    euler: Refactor 12.hs

--- euler/12.hs
+++ euler/12.hs
@@ -3,25 +3,26 @@
 main :: IO ()
 main = print . head . filter ((> 500) . countDivisor) . map triNum $ [1 ..]
 
-triNum :: Int -> Int
+triNum :: Integer -> Integer
 triNum n = div (n * (n + 1)) 2
 
-countDivisor :: Int -> Int
+countDivisor :: Integer -> Integer
 countDivisor n = foldr fn 1 (factorize n)
   where
     fn (_, b) c = (b + 1) * c
 
-factorize :: Int -> [(Int, Int)]
+factorize :: Integer -> [(Integer, Integer)]
 factorize n = factor' n 2 []
   where
-    factor' :: Int -> Int -> [(Int, Int)] -> [(Int, Int)]
+    factor' :: Integer -> Integer -> [(Integer, Integer)] -> [(Integer, Integer)]
     factor' 1 _ xs = xs
     factor' m p xs
+        | p * p > m = (m, 1) : xs
         | mod m p == 0 = factor' m' (p + 1) ((p, x) : xs)
         | otherwise = factor' m (p + 1) xs
       where
         (m', x) = countOrder m p 0
-        countOrder :: Int -> Int -> Int -> (Int, Int)
+        countOrder :: Integer -> Integer -> Integer -> (Integer, Integer)
         countOrder a b acc
             | mod a b == 0 = countOrder (div a b) b (acc + 1)
             | otherwise = (a, acc)
