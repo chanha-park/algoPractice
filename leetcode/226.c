d4ad67ca68c1fa40995ec75424ad78209b739b25
chanhpar <chanhpar@student.42seoul.kr>
2023-02-18 16:09:16 +0900

    leetcode: solve 226

---
+++ leetcode/226.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/* Definition for a binary tree node. */
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    if (root != NULL) {
+        invertTree(root->left);
+        invertTree(root->right);
+        struct TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+    }
+    return (root);
+}
