26b222ac1b7266f1bb46dd5e9e4260de97c7e015
chanha-park <chanhpar@student.42seoul.kr>
2023-02-22 15:35:42 +0900

    leetcode: refactor 1011

--- leetcode/1011.c
+++ leetcode/1011.c
@@ -2,10 +2,7 @@ int countDays(int* weights, int weightsSize, int cap) {
     int count = 1;
     int sum = 0;
     for (int i = 0; i < weightsSize; ++i) {
-        if (weights[i] > cap) {
-            return (50000);
-        }
-        else if (sum + weights[i] <= cap) {
+        if (sum + weights[i] <= cap) {
             sum += weights[i];
         } else {
             ++count;
@@ -16,15 +13,15 @@ int countDays(int* weights, int weightsSize, int cap) {
 }
 
 int shipWithinDays(int* weights, int weightsSize, int days){
-    int left = 0;
-    int right = -1;
+    int left = -1;
+    int right = 0;
     int mid;
     int ans;
     for (int i = 0; i < weightsSize; ++i) {
-        if (weights[i] > right)
-            right = weights[i];
+        if (weights[i] > left)
+            left = weights[i];
+        right += weights[i];
     }
-    right *= 1 + weightsSize / days;
     ans = right;
     while (left < right) {
         mid = (left + right) / 2;
@@ -36,21 +33,4 @@ int shipWithinDays(int* weights, int weightsSize, int days){
         }
     }
     return (ans);
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-    int weightsSize;
-    int *weights;
-    int days;
-
-    scanf("%d", &weightsSize);
-    weights = malloc(sizeof(int) * weightsSize);
-    for (int i = 0; i < weightsSize; ++i)
-        scanf("%d", &weights[i]);
-    scanf("%d", &days);
-    printf("%d", shipWithinDays(weights, weightsSize, days));
-    return (0);
 }
