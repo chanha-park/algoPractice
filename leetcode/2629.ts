c83cd9983cd78e6e6019c5f042f52c99893bb8ee
chanhpar <chanhpar@student.42seoul.kr>
2023-06-30 15:42:40 +0900

    refactor

--- leetcode/2629.ts
+++ leetcode/2629.ts
@@ -1,13 +1,6 @@
 type F = (x: number) => number;
 
-const compose = (functions: F[]): F => {
-    const id = (x) => x;
-    return functions.reduce((a, b) => (x) => a(b(x)), id);
-};
-
-// function foo<T>(x :T){console.log(x)}
-
-// Array<F>.reduce(callbackfn: (previousValue: F, currentValue: F, currentIndex: number, array: F[]) => F, initialValue: F): F
+const compose = (functions: F[]): F => functions.reduce((a, b) => (x) => a(b(x)), <T>(x: T) => x);
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
