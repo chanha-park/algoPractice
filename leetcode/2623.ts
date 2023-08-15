a86bcb59acb009732126aefe2b698a3f920f8bbf
chanhpar <chanhpar@student.42seoul.kr>
2023-08-15 21:19:59 +0900

    leetcode: 2623

---
+++ leetcode/2623.ts
@@ -0,0 +1,20 @@
+type Fn = (...params: any) => any
+
+function memoize(fn: Fn): Fn {
+    const memo = new Map();
+    return function(...args) {
+        const key = JSON.stringify(args);
+        return memo.get(key) ?? memo.set(key, fn(...args)).get(key);
+    }
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
